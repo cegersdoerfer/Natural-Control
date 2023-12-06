037082bc537c8f976ee942341143a33fb428a47b
cegersdoerfer <cegersdoerfer@gmail.com>
2023-12-06 00:38:24 -0500

    add water

--- raspberry-pi/gpio_control.py
+++ raspberry-pi/gpio_control.py
@@ -1,16 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-switch_pin = 8
-
+switch_pin1 = 8
+switch_pin2 = 10
+water_pin = 16
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(switch_pin, GPIO.OUT)
+GPIO.setup(switch_pin1, GPIO.OUT)
+GPIO.setup(switch_pin2, GPIO.OUT)
+GPIO.setup(water_pin, GPIO.OUT)
 
 while True:
     current_time = time.localtime()
-    if current_time.tm_hour <= 8 and current_time.tm_hour >= 20:
-        GPIO.output(switch_pin, GPIO.HIGH)
+    if current_time.tm_hour < 8 or current_time.tm_hour >= 22:
+        GPIO.output(switch_pin1, GPIO.HIGH)
+        GPIO.output(switch_pin2, GPIO.HIGH)
+        GPIO.output(water_pin, GPIO.HIGH)
     else:
-        GPIO.output(switch_pin, GPIO.LOW)
+        GPIO.output(switch_pin1, GPIO.LOW)
+        GPIO.output(switch_pin2, GPIO.LOW)
     time.sleep(60)
